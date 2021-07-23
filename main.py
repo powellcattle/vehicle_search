dfa45c5b982cab2db90db26e07b0f75d87b5c99a
powellcattle <43384789+powellcattle@users.noreply.github.com>
2021-07-23 14:12:10 -0500

    corrected iteration

--- main.py
+++ main.py
@@ -11,28 +11,37 @@ try:
 except OSError as e:
     sys.intern(e.strerror)
 
+# for search_list in search_names_json:
+#     search_name = search_list[0].get("search_name")
+#     for i in range(1, len(search_list)):
+#         site_site = search_list[i][0]
+#         site_site.get("search_site")
+#         pass
+
 if search_names_json is None:
     sys.exit((-1))
 
 results_all = list(dict())
 results_all_typed = list(dict())
 
-for search_name in search_names_json['allsearches']:
-    name = search_name.get('name')
-    search_type = search_name.get('search_site')
+for search_list in search_names_json:
+    name = search_list[0].get("search_name")
+    for i in range(1, len(search_list)):
+        site = search_list[i][0]
+        search_type = site.get("search_site")
 
-    if search_type == 'craigslist':
-        search_craigslist(search_name, results_all, results_all_typed)
-    elif search_type == 'autotrader':
-        search_autotrader(search_name, results_all, results_all_typed)
-    else:
-        continue
+        if search_type == 'craigslist':
+            search_craigslist(site, results_all, results_all_typed)
+        elif search_type == 'autotrader':
+            search_autotrader(site, results_all, results_all_typed)
+        else:
+            continue
 
-try:
-    f = open('C:/dev/reports/test.html', 'w', encoding='utf8')
-    text = write_html(results_all_typed, results_all)
-    f.write(text)
-    f.close()
+    try:
+        f = open('C:/dev/reports/' + name + '.html', 'w', encoding='utf8')
+        text = write_html(results_all_typed, results_all)
+        f.write(text)
+        f.close()
 
-except OSError as e:
-    print(e)
+    except OSError as e:
+        print(e)
