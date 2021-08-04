64d4c1d014ca7da3f1b5f5f60db8569cf87e6369
spowell <spowell@powellcattle.com>
2021-08-04 16:19:03 -0500

    Added some comments

--- main.py
+++ main.py
@@ -6,10 +6,13 @@ import sys
 from myemail import Mail
 from utils.helper import write_html, search_oodle, search_craigslist, search_autotrader
 
+# need to remove all .html files from REPORT DIR before starting
+
 REPORT_DIR = '../../dev/reports/'
 DATA_DIR = './meta-data/'
 
-search_names_json = dict()
+# read external search criteria
+search_names_json = None
 try:
     f = open(DATA_DIR + 'site_searches.json')
     search_names_json = json.load(f)
@@ -20,6 +23,7 @@ except OSError as e:
 if search_names_json is None:
     sys.exit((-1))
 
+# each search name can have ..n searches on various search sites
 for search_list in search_names_json:
 
     name = search_list[0].get("search_name")
@@ -28,6 +32,7 @@ for search_list in search_names_json:
 
     for idx in range(1, len(search_list)):
         site = search_list[idx][0]
+        # represents the site e.g., craigslist or autotrader...
         search_type = site.get("search_site")
 
         if search_type == 'craigslist':
@@ -43,6 +48,7 @@ for search_list in search_names_json:
             continue
 
     try:
+        # after all the criteria is completed for a site name, write the results to HTML
         f = open(REPORT_DIR + name + '.html', 'w', encoding='utf8')
         text = write_html(results_all_typed, results_all)
         f.write(text)
